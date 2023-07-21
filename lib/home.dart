4441a519f79c746da26d9df4b56a487b48b83ff5
Mad_Phinit <rungrit2005@gmail.com>
2023-07-21 23:40:31 +1000

    Added title comment

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,10 @@
+// For the home page. When you clicked the 'Home' at the bottom of the app
+// contains everything that you can see when you click home button
+// TODO: Finish home page
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' as parser;
+import 'package:http/http.dart' as http; // Required to get data from website
+import 'package:html/parser.dart' as parser; // Also required
 
 class AQIRipper extends StatefulWidget {
   const AQIRipper({super.key});
@@ -11,6 +15,7 @@ class AQIRipper extends StatefulWidget {
 
 class _AQIWidgetState extends State<AQIRipper> {
   String _aqi = 'Calculating...';
+  String _message = 'Loading..';
 
   @override
   void initState() {
@@ -18,22 +23,22 @@ class _AQIWidgetState extends State<AQIRipper> {
     fetchData('https://www.iqair.com/australia',
         'aqi-box-green description__header__number iqair-aqi-pill');
   }
-
   // This function fetchData from the website.
   // It needed the url or the website and the class name of that data you want to take
   Future<void> fetchData(url, urlclassName) async {
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      // Get the content from iqair.com
+      // Get the content from the website you want
       final document = parser.parse(response.body);
       // Find the specific element that contains the AQI number then take it :)
-      // Illegal? I think not
       final elements = document.getElementsByClassName(urlclassName);
       if (elements.isNotEmpty) {
         final aqiNumber = elements.first.text.trim();
         setState(() {
           _aqi = aqiNumber;
         });
+        textFeedback(
+            aqiNumber); // Call textFeedback after it got the AQI number
       } else {
         setState(() {
           _aqi = 'Data not found!';
@@ -46,6 +51,39 @@ class _AQIWidgetState extends State<AQIRipper> {
     }
   }
 
+  // This function check the aqi scale and find out that you should go outside or not
+  // Required string number and it changes the _message variable.
+  Future<void> textFeedback(String number) async {
+    try {
+      int aqi = int.parse(number);
+      if (aqi < 35) {
+        setState(() {
+          _message = "It's a good day to go outside";
+        });
+      } else if (aqi >= 35 && aqi < 60) {
+        setState(() {
+          _message = "Recommended to wear a mask";
+        });
+      } else if (aqi >= 60 && aqi < 100) {
+        setState(() {
+          _message = "Sensitive people should avoid going outside";
+        });
+      } else if (aqi >= 100 && aqi < 160) {
+        setState(() {
+          _message = "Wear a mask if you can";
+        });
+      } else {
+        setState(() {
+          _message = "You should avoid going outside today";
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _message = "Loading...";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -53,9 +91,13 @@ class _AQIWidgetState extends State<AQIRipper> {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Text(
-          'AQI: $_aqi',
+          'AQI: $_aqi ',
           style: const TextStyle(fontSize: 30, color: Colors.white),
-        )
+        ),
+        Text(
+          '$_message',
+          style: const TextStyle(fontSize: 20, color: Colors.white),
+        ),
       ],
     );
   }
@@ -80,13 +122,14 @@ class HomeScreen extends StatelessWidget {
           ),
           child: const Column(
             children: [
+              AQIRipper(),
               Align(
                 alignment: Alignment.centerLeft,
                 // TODO: Don't forget this!!!
                 child: Text('',
-                    style: TextStyle(color: Colors.white, fontSize: 10)),
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                    textAlign: TextAlign.center),
               ),
-              AQIRipper()
             ],
           ),
         ),
