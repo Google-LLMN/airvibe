9742903659532cec388c57fe98b24512e480368d
Mad_Phinit <rungrit2005@gmail.com>
2023-07-21 23:39:56 +1000

    Added comment

--- lib/add.dart
+++ lib/add.dart
@@ -1,15 +1,19 @@
-import 'package:flutter/material.dart';
+// For the add page. When you clicked the big plus green button
+// TODO: Finish add page
 
+import 'package:flutter/material.dart';
 
 class AddScreen extends StatelessWidget {
   const AddScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Color.fromARGB(255, 44, 77, 138),),
-      backgroundColor: Color.fromARGB(255, 32, 56, 100),
-      body: Center(
-        child: Text('CommingSoon'),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 44, 77, 138),
+      ),
+      backgroundColor: const Color.fromARGB(255, 32, 56, 100),
+      body: const Center(
+        child: Text('Coming Soon...', style: TextStyle(color: Colors.white)),
       ),
     );
   }
