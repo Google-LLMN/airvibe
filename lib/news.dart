04bb7d0520c1067e5dbad266d65c715ae79db6bc
Mad_Phinit <rungrit2005@gmail.com>
2023-08-21 23:14:02 +1000

    small adjustments

--- lib/news.dart
+++ lib/news.dart
@@ -1,7 +1,24 @@
 // TODO: Finish news page
 
 import 'package:flutter/material.dart';
+import 'Data/airvibe_methods.dart';
+import 'home.dart' show AQIRipper;
 
+class BingNewsPaper extends StatefulWidget {
+  const BingNewsPaper({super.key});
+
+  @override
+  State<BingNewsPaper> createState() => _BingNewsPaperState();
+}
+
+class _BingNewsPaperState extends State<BingNewsPaper> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
 
 class NewsScreen extends StatelessWidget {
   const NewsScreen({super.key});
